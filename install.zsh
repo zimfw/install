93f083ea431b91ae9e7bdf89bc53a43ee4b1f89c
Eric Nielsen <eric@amalgamar.com.br>
2019-12-03 18:37:41 -0500

    Remove options in zshrc template
    that are already in the environment module. We don't want to scary new
    Zsh users with a lot of options at first.  :- )

--- install.zsh
+++ install.zsh
@@ -144,59 +144,11 @@ ZTEMPLATES[zshrc]="#
 # -----------------
 
 #
-# Changing directories
-#
-
-# Perform cd to a directory if the typed command is invalid, but is a directory.
-setopt AUTO_CD
-
-# Make cd push the old directory to the directory stack.
-setopt AUTO_PUSHD
-
-# Don't push multiple copies of the same directory to the stack.
-setopt PUSHD_IGNORE_DUPS
-
-# Don't print the directory stack after pushd or popd.
-setopt PUSHD_SILENT
-
-# Have pushd without arguments act like \`pushd \${HOME}\`.
-setopt PUSHD_TO_HOME
-
-#
-# Expansion and globbing
-#
-
-# Treat \`#\`, \`~\`, and \`^\` as patterns for filename globbing.
-setopt EXTENDED_GLOB
-
-#
 # History
 #
 
-# The file to save the history in.
-HISTFILE=\${ZDOTDIR:-\${HOME}}/.zhistory
-
-# The maximum number of events stored internally and saved in the history file.
-HISTSIZE=10000
-SAVEHIST=10000
-
-# Don't display duplicates when searching the history.
-setopt HIST_FIND_NO_DUPS
-
-# Don't enter immediate duplicates into the history.
-setopt HIST_IGNORE_DUPS
-
 # Remove older command from the history if a duplicate is to be added.
 setopt HIST_IGNORE_ALL_DUPS
-
-# Remove commands from the history that begin with a space.
-setopt HIST_IGNORE_SPACE
-
-# Don't execute the command directly upon history expansion.
-setopt HIST_VERIFY
-
-# Cause all terminals to share the same history 'session'.
-setopt SHARE_HISTORY
 
 #
 # Input/output
@@ -211,33 +163,9 @@ bindkey -e
 # Customize spelling correction prompt.
 #SPROMPT='zsh: correct %F{red}%R%f to %F{green}%r%f [nyae]? '
 
-# Allow comments starting with \`#\` in the interactive shell.
-setopt INTERACTIVE_COMMENTS
-
-# Disallow \`>\` to overwrite existing files. Use \`>|\` or \`>!\` instead.
-setopt NO_CLOBBER
-
 # Remove path separator from WORDCHARS.
 WORDCHARS=\${WORDCHARS//[\\/]}
 
-#
-# Job control
-#
-
-# Resume an existing job before creating a new one.
-setopt AUTO_RESUME
-
-# List jobs in verbose format by default.
-setopt LONG_LIST_JOBS
-
-# Prevent background jobs being given a lower priority.
-setopt NO_BG_NICE
-
-# Prevent status report of jobs on shell exit.
-setopt NO_CHECK_JOBS
-
-# Prevent SIGHUP to jobs on shell exit.
-setopt NO_HUP
 
 # --------------------
 # Module configuration
