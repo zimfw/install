e222ee1ad33b6da2c46791470f0caefa45a7cd2c
Eric Nielsen <4120606+ericbn@users.noreply.github.com>
2025-02-24 08:59:48 -0500

    Prefer init without -q
    zimfw already shows relevant information with the init action, tailored
    for the terminal startup. There's potential to use that to show even
    more relevant information to users, so let's not disable it by default.

--- install.zsh
+++ install.zsh
@@ -303,7 +303,7 @@ if [[ ! -e \${ZIM_HOME}/zimfw.zsh ]]; then
 fi
 # Install missing modules, and update \${ZIM_HOME}/init.zsh if missing or outdated.
 if [[ ! \${ZIM_HOME}/init.zsh -nt \${ZIM_CONFIG_FILE:-\${ZDOTDIR:-\${HOME}}/.zimrc} ]]; then
-  source \${ZIM_HOME}/zimfw.zsh init -q
+  source \${ZIM_HOME}/zimfw.zsh init
 fi
 # Initialize modules.
 source \${ZIM_HOME}/init.zsh
